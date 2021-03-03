848e334b9570f7e57a12a24569761ac4f80d3fe4
popkorn96 <mpaloma2014@gmail.com>
2021-03-02 18:02:31 -0600

    added buttons

--- sfa_online/src/components/NavBar.js
+++ sfa_online/src/components/NavBar.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-export default function NavBar() {
+export default function EnglishNavBar() {
     return (
         <div>
             <nav id="nav-bar">
                 <a className="nav-title">San Francisco de Asis</a>
+                <ol>
+                    <button class="nav-item active" >Home</button>
+                    <button class="nav-item active" >Events</button>
+                    <button class="nav-item active" >Gallery</button>
+                    <button class="nav-item active" >About Us</button>
+                </ol>
             </nav>
         </div>
     )
