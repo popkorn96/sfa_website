9de9baac5e35121574f71e18d84fd05c53a995c1
popkorn96 <mpaloma2014@gmail.com>
2021-03-03 14:07:36 -0600

    adding routes

--- sfa_online/src/App.js
+++ sfa_online/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
-import EnglishNavBar from './components/EnglishNavBar';
+import EnglishNavBar from './en-components/EnglishNavBar';
 
 
 function App() {
---
+++ sfa_online/src/en-components/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+
+export default function Routes() {
+    return (
+        <Switch>
+            <div>
+                <Route exact path="/" component={Main} />
+                <Route exact path="/admin-login" component={} />
+                <Route exact path="/admin-edit" component={} />
+                <Route exact path="/en-home" component={} />
+                <Route exact path="/en-events" component={} />
+                <Route exact path="/en-gallery" component={} />
+                <Route exact path="/en-about-us" component={} />
+                <Route exact path="/sp-pagína-principal" component={} />
+                <Route exact path="/sp-eventos" component={} />
+                <Route exact path="/sp-galería" component={} />
+                <Route exact path="/sp-sobre-nosotros" component={} />
+            </div>
+        </Switch>
+    )
+}
