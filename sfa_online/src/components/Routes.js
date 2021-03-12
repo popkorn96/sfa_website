9070fa7cd1927118f30192dfc2affe341e8f1da6
popkorn96 <mpaloma2014@gmail.com>
2021-03-12 11:27:24 -0600

    creating containers

--- sfa_online/src/components/Routes.js
+++ sfa_online/src/components/Routes.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Switch, Route, Router} from 'react-router-dom';
 import Main from './Main';
 import AdminLogin from './AdminLogin';
 import AdminEdit from './AdminEdit';
+import EnHome from '../en-containers/EnHome'
 
 
 export default function Routes() {
     return (
-        <Switch>
-            <div>
-                {/* <Route exact path="/" component={Main} /> */}
-                <Route exact path="/admin-login" component={AdminLogin} />
-                <Route exact path="/admin-edit" component={} />
-                <Route exact path="/en-home" component={} />
-                <Route exact path="/en-events" component={} />
-                <Route exact path="/en-gallery" component={} />
-                <Route exact path="/en-about-us" component={} />
-                <Route exact path="/sp-pagína-principal" component={} />
-                <Route exact path="/sp-eventos" component={} />
-                <Route exact path="/sp-galería" component={} />
-                <Route exact path="/sp-sobre-nosotros" component={} />
-            </div>
-        </Switch>
+        <Router>
+            <Switch>
+                    <div>
+                        <Route exact path="/" component={Main} />
+                        <Route exact path="/admin-login" component={AdminLogin} />
+                        <Route exact path="/admin-edit" component={AdminEdit} />
+                        <Route exact path="/en-home" component={EnHome} />
+                        <Route exact path="/en-events" component={} />
+                        <Route exact path="/en-gallery" component={} />
+                        <Route exact path="/en-about-us" component={} />
+                        <Route exact path="/sp-pagína-principal" component={} />
+                        <Route exact path="/sp-eventos" component={} />
+                        <Route exact path="/sp-galería" component={} />
+                        <Route exact path="/sp-sobre-nosotros" component={} />
+                    </div>
+                </Switch>
+        </Router>
     )
 }
