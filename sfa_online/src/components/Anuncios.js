0b881fac01b27af3edab03ff3af4fee3f08d9d18
popkorn96 <mpaloma2014@gmail.com>
2021-03-18 12:47:04 -0500

    Added announcement component

---
+++ sfa_online/src/components/Anuncios.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Anuncios() {
+    return (
+        <div>
+            <h1>Anuncios</h1>
+        </div>
+    )
+}
--- sfa_online/src/en-containers/EnEvents.js
+++ sfa_online/src/en-containers/EnEvents.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 
 export default function EnEvents() {
-    let { path, url } = useRouteMatch();
     return (
         <div>
             <h1>English Events</h1>
-            <h2><a href={`${url}/rendering`}></a></h2>
         </div>
     )
 }
