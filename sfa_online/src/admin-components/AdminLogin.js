563c20dd21a298cd3948983c37959bfdc44b175b
popkorn96 <mpaloma2014@gmail.com>
2021-03-23 17:59:22 -0500

    erased trial

--- sfa_online/src/admin-components/AdminLogin.js
+++ sfa_online/src/admin-components/AdminLogin.js
@@ -2,34 +2,19 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Login.css';
 
-// async function loginUser(credentials) {
-//   return fetch('http://localhost:8080/admin-login', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(credentials)
-//   })
-//     .then(data => data.json())
-//  }
+export default function AdminLogin() {
 
-export default function AdminLogin({setToken}) {
-  const handleSubmit = async e => {
-    console.log(`fetching..`)
-  }
-  const [username, setUserName] = useState();
-  const [password, setPassword] = useState();
     return (
         <div className="login-wrapper">
         <h1>Please Log In</h1>
-        <form onSubmit={handleSubmit}>
+        <form >
           <label>
             <p>Username</p>
-            <input type="text" onChange={e => setUserName(e.target.value)}/>
+            <input type="text" />
           </label>
           <label>
             <p>Password</p>
-            <input type="password" onChange={e => setPassword(e.target.value)}/>
+            <input type="password" />
           </label>
           <div>
             <button type="submit">Submit</button>
