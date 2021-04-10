95defc4ad8ca63ba537916e28d434a4c157b6edf
popkorn96 <mpaloma2014@gmail.com>
2021-04-10 16:30:39 -0500

    centered navbar.brand and added size styling

--- sfa_online/src/components/NavBar.js
+++ sfa_online/src/components/NavBar.js
@@ -6,13 +6,13 @@ import {Navbar, Nav, NavDropdown} from 'react-bootstrap/';
 export const MainNavigationBar = (props) => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/">
+        <Navbar.Brand className="center-navbar"href="/">
             <img
             src="./logo1.png"
-            width="50"
-            height="50"
+            width="60"
+            height="60"
             className="d-inline-block align-top"
-            alt=""
+            alt="SFA logo"
             />{' '}San Francisco de Asís</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
