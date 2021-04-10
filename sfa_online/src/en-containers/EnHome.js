bf528585056a805973dd8ddb7869b82b56563792
popkorn96 <mpaloma2014@gmail.com>
2021-04-10 16:20:33 -0500

    added containers to en-home

--- sfa_online/src/en-containers/EnHome.js
+++ sfa_online/src/en-containers/EnHome.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Announcements from '../components/Announcements';
-import EnglishNavigationBar from '../components/EnNavBar'
+import EnglishNavigationBar from '../components/EnNavBar';
+import {Container, Row, Col} from 'react-bootstrap';
 
 export default function EnHome() {
     return (
-        <div>
-            <EnglishNavigationBar />
-            <h1>English Home</h1>
-            <Announcements/>
-        </div>
+        <Container fluid>
+                <EnglishNavigationBar /><br/>
+            <Row>
+                <Col><Announcements/></Col>
+                <Col xs={6}><h1>English Home</h1></Col>
+                <Col><h4>Upcoming Events...</h4></Col>
+            </Row>
+        </Container>
     )
 }
