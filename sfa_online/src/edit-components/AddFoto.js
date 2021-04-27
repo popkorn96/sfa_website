518c95d06abd15ab6ea5ea0eac0291e0add20d53
popkorn96 <mpaloma2014@gmail.com>
2021-04-27 18:30:14 -0500

    created add photo

---
+++ sfa_online/src/edit-components/AddFoto.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AddFoto extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
