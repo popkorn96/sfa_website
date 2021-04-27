975d14416af11bf38b5d091c53d2275660505b9a
popkorn96 <mpaloma2014@gmail.com>
2021-04-27 18:40:11 -0500

    added rest of form for photos

--- sfa_online/src/edit-components/AddFoto.js
+++ sfa_online/src/edit-components/AddFoto.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import {Form} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
+import * as Icon from 'react-feather';
+
 
 export default class AddFoto extends Component {
     render() {
         return (
             <div>
+                <br/><h1>Foto</h1>
                 <Form>
-                <Form.Group>
-                    <Form.File id="exampleFormControlFile1" label="Example file input" />
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Título</Form.Label>
+                    <Form.Control type="email" />
                 </Form.Group>
-                </Form>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Descripción de Foto</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.File id="exampleFormControlFile1" label="Escoger fotos para subir a galería..." />
+                </Form.Group>
+                <Button>Subir</Button>
+                </Form><br/>
             </div>
         )
     }
--- sfa_online/src/edit-components/AddPhoto.js
+++ sfa_online/src/edit-components/AddPhoto.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import {Form} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
+import * as Icon from 'react-feather';
 
 export default class AddPhoto extends Component {
     render() {
         return (
             <div>
+                <br/><h1>Photo</h1>
                 <Form>
-                <Form.Group>
-                    <Form.File id="exampleFormControlFile1" label="Example file input" />
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" />
                 </Form.Group>
-                </Form>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Photo Description</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Form.Group>
+                    <Form.File id="exampleFormControlFile1" label="Choose photo to upload to gallery..." />
+                </Form.Group>
+                <Button>Submit</Button>
+                </Form><br/>
             </div>
         )
     }
