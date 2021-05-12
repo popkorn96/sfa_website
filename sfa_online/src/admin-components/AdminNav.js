c71a97ba828c6e9bd3b32a18b739936dcf00abd1
popkorn96 <mpaloma2014@gmail.com>
2021-05-12 17:33:37 -0500

    added readme

---
+++ sfa_online/src/admin-components/AdminNav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap/';
+import * as Icon from 'react-feather';
+
+
+export const MainNavigationBar = (props) => {
+    return (
+        <Navbar className="main-nav" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand className="center-navbar"href="/">
+            <img
+            src="./logo1.png"
+            width="60"
+            height="60"
+            className="d-inline-block align-top"
+            alt="SFA"
+            />{' '}San Francisco de Asís</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav>
+            <Nav.Link href="/en-home">English</Nav.Link>
+            <Nav.Link eventKey={2} href="/sp-pagina-principal">Español</Nav.Link>
+            </Nav>
+            <Nav>
+                <Nav.Link href="/admin-logout"><Icon.LogIn/></Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+    );
+};
+export default (MainNavigationBar)
