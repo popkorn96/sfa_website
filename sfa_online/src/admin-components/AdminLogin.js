ee7159a4b28fca99654dd651b32e01f3d071b6ea
popkorn96 <mpaloma2014@gmail.com>
2021-06-05 20:30:24 -0500

    removing notes

--- sfa_online/src/admin-components/AdminLogin.js
+++ sfa_online/src/admin-components/AdminLogin.js
@@ -10,7 +10,6 @@ export default function AdminLogin() {
         <div className="login-wrapper"><br/>
         <p>See if you can add omniauth to the login portal</p>
         <h1>Administrative Login</h1>
-        <i>This is the intended login for San Francisco de Asis Administration ONLY</i><br/>
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Administrative Email</Form.Label>
