44f62b65cdc99ec83b1fc5b5bc1fc7a6ea8b5e04
popkorn96 <mpaloma2014@gmail.com>
2021-06-10 14:18:25 -0500

    alignment

--- sfa_online/src/components/MainNavigationBar.js
+++ sfa_online/src/components/MainNavigationBar.js
@@ -17,7 +17,7 @@ export const MainNavigationBar = (props) => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-            </Nav>
+            </Nav>  
             <Nav>  
             <Nav.Link href="/en-home">English</Nav.Link>
             <Nav.Link eventKey={2} href="/sp-pagina-principal">Español</Nav.Link>
